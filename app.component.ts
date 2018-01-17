824a0168e28e61bab1dcaa4859d67f7da5f624b3
PhanTien Dung <d_phan@visionet.local>
2018-01-17 15:29:00 +0700

    first-routing

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bela';
+  obj = {
+  	id: "1",
+  	name: "betho"
+  }
+  ly = ["la","tho","ly"];
+  isTrue = true;
+  la = "la";
+}
