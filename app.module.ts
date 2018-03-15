9906c82408ac80cba1aee8c28f2be2f9674cfb37
vika41130 <vika41130@gmail.com>
2018-03-15 22:05:33 +0700

    3 files

--- app.module.ts
+++ app.module.ts
@@ -7,6 +7,12 @@ import { ElementRefComponent } from './element-ref/element-ref.component';
 import { TemplateRefComponent } from './template-ref/template-ref.component';
 import { ViewRefComponent } from './view-ref/view-ref.component';
 import { ViewContainerRefComponent } from './view-container-ref/view-container-ref.component';
+import { NgTemplateOutletComponent } from './ng-template-outlet/ng-template-outlet.component';
+import { NgComponentOutletComponent } from './ng-component-outlet/ng-component-outlet.component';
+
+// import { HelloWorldComponent } from './ng-component-outlet/ng-component-outlet.component';
+// import { CompleteComponent } from './ng-component-outlet/ng-component-outlet.component';
+import { OtherModuleComponent } from './ng-component-outlet/ng-component-outlet.component';
 
 
 @NgModule({
@@ -15,7 +21,17 @@ import { ViewContainerRefComponent } from './view-container-ref/view-container-r
     ElementRefComponent,
     TemplateRefComponent,
     ViewRefComponent,
-    ViewContainerRefComponent
+    ViewContainerRefComponent,
+    NgTemplateOutletComponent,
+    NgComponentOutletComponent,
+    // HelloWorldComponent,
+    // CompleteComponent,
+    OtherModuleComponent,
+  ],
+  entryComponents: [
+    // HelloWorldComponent,
+    // CompleteComponent,
+    OtherModuleComponent,
   ],
   imports: [
     BrowserModule
--- ng-component-outlet.component.ts
+++ ng-component-outlet.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, Injectable, Injector, ReflectiveInjector,
+  Compiler, NgModuleFactory
+} from '@angular/core';
+
+// core
+// @Component({
+//   selector: 'app-hello-world',
+//   template: 'This is ng-component-outlet'
+// })
+// export class HelloWorldComponent {
+// }
+
+// @Component({
+//   selector: 'app-ng-component-outlet',
+//   template: `
+//     <ng-container *ngComponentOutlet="HelloWorldComponent">
+//     </ng-container>
+//   `,
+//   styleUrls: ['./ng-component-outlet.component.css']
+// })
+// export class NgComponentOutletComponent implements OnInit {
+// This field is necessary to expose HelloWorld to the template.
+//   HelloWorldComponent = HelloWorldComponent;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+// injectable
+// @Injectable()
+// class Greeter {
+//   suffix = '!';
+// }
+
+// @Component({
+//   selector: 'app-complete-component',
+//   template: `Complete: <ng-content></ng-content>-----<ng-content></ng-content>{{ greeter.suffix }}`
+// })
+// export class CompleteComponent {
+//   constructor(public greeter: Greeter) { }
+// }
+
+// @Component({
+//   selector: 'app-ng-component-outlet',
+//   template: `
+//     <ng-container *ngComponentOutlet="CompleteComponent;
+//     injector: myInjector;  content: myContent">
+//     </ng-container>
+//   `
+// })
+// export class NgComponentOutletComponent implements OnInit {
+//   CompleteComponent = CompleteComponent;
+//   myInjector: Injector;
+
+//   myContent = [[document.createTextNode('Ahoj')], [document.createTextNode('Svet')]];
+
+//   constructor(injector: Injector) {
+//     this.myInjector = ReflectiveInjector.resolveAndCreate([Greeter], injector);
+//   }
+
+//   ngOnInit() {
+//   }
+// }
+
+// ngModuleFactory
+@Component({ selector: 'app-other-module-component', template: `Other Module Component!` })
+export class OtherModuleComponent {
+}
 
 @Component({
   selector: 'app-ng-component-outlet',
-  templateUrl: './ng-component-outlet.component.html',
-  styleUrls: ['./ng-component-outlet.component.css']
+  template: `
+    <ng-container *ngComponentOutlet="OtherModuleComponent;
+      ngModuleFactory: myModule;"></ng-container>`
 })
 export class NgComponentOutletComponent implements OnInit {
+  OtherModuleComponent = OtherModuleComponent;
+  myModule: NgModuleFactory<any>;
 
-  constructor() { }
+  constructor(compiler: Compiler) {
+    this.myModule = compiler.compileModuleSync(OtherModuleComponent);
+  }
 
   ngOnInit() {
   }
-
 }
