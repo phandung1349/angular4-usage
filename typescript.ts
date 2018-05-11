c8df554b8b3f44feee7d3fd146280f2f6345e007
vika41130 <vika41130@gmail.com>
2018-05-11 09:27:06 +0700

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -6,4 +6,6 @@
 1. Object.entries(obj).forEach(
   ([key, value]) => console.log(key, value)
 );
-1. data type: string, number, boolean, undefined, null
+1. data type: string, number, boolean, undefined, null, union
+  let a: number | string | null | undefined;
+1. 
