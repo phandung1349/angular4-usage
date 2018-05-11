fccdbcd1513855a23d74a8fd28f5cd56d31a105b
vika41130 <vika41130@gmail.com>
2018-05-11 09:17:12 +0700

    Create typescript.ts

---
+++ typescript.ts
@@ -0,0 +1,9 @@
+1. forEach(data=>{})
+1. forEach((key,value)=>{})
+1. Object.keys(obj).forEach(key => {
+  let value = obj[key];
+});
+1. Object.entries(obj).forEach(
+  ([key, value]) => console.log(key, value)
+);
+1. data type: string, number, boolean, undefined, null
